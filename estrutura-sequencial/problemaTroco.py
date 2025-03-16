f8ccb9371adbda67c5e341f8d938236482efa812
Fuyuki2 <erikjhonathan786@gmail.com>
2025-03-15 23:42:01 -0300

    Update problemaTroco.py

--- estrutura-sequencial/problemaTroco.py
+++ estrutura-sequencial/problemaTroco.py
@@ -2,6 +2,5 @@ preco = float(input("Digite o preco do produto: "))
 unidade = int(input("Digite a quanitdade de produtos: "))
 dinheiro = float(input("Digite o valor recebido: "))
 
-total = preco * unidade
-troco = dinheiro - total
+troco = dinheiro - (preco * unidade)
 print(f"O valor do troco e de {troco}")
